2d9ad42b9760cd66a4a90bf7e21283b5b9e33511
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-05 23:15:18 +0000

    ddd

--- app.py
+++ app.py
@@ -7,12 +7,13 @@ openai.api_key =  os.getenv("APIKEY")
 
 query = st.text_input("Input Query")
 
-response = openai.Image.create(
-  prompt=query,
-  n=1,
-  size="1024x1024"
-)
-image_url = response['data'][0]['url']
-urllib.request.urlretrieve(img_url, 'img.png')
-img = Image.open("img.png")
-st.image(img)
+if st.button("Generate Diagram"):
+    response = openai.Image.create(
+    prompt=query,
+    n=1,
+    size="1024x1024"
+    )
+    image_url = response['data'][0]['url']
+    urllib.request.urlretrieve(img_url, 'img.png')
+    img = Image.open("img.png")
+    st.image(img)
