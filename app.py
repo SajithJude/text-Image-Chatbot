56bd786f031d620bb5b6ef8eec6715a56d855558
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-05 23:12:14 +0000

    sss

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import streamlit as st
 import os
+import urllib.request
 import openai
 
 
 query = st.text_input("Input Query")
 
 response = openai.Image.create(
-  prompt="a white siamese cat",
+  prompt=query,
   n=1,
   size="1024x1024"
 )
 image_url = response['data'][0]['url']
-
- 
+urllib.request.urlretrieve(img_url, 'img.png')
+img = Image.open("img.png")
+st.image(img)
